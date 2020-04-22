0efd3533d7b7830b3c7c97c10eaee87f8e1af497
Alex King <alexking124@gmail.com>
2020-04-21 22:37:49 -0700

    Try again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 async function run() {
-    await exec.exec('pwd')
+    // await exec.exec('pwd')
     await exec.exec('which mint')
     await exec.exec('brew outdated mint')
     await exec.exec('brew install mint')
