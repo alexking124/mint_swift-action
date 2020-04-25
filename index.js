b1f8a8c1911fc31e759fe4fa27c992425b5bec86
Alex King <alexking124@gmail.com>
2020-04-24 23:10:08 -0700

    Testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const exec = require('@actions/exec');
 async function run() {
     try {
         // await exec.exec('pwd')
-        await exec.exec('which log')
+        await exec.exec('which log').catch((e) => { console.error(e.message) })
         await exec.exec('brew outdated mint')
         await exec.exec('brew install mint')
     }
